d6372b25b982663c87ec9a143e055294ce8464fa
Garden <847686279@qq.com>
2018-06-08 20:04:49 +0800

    完成控制器异常方法处理机制集成

--- src/main/java/com/garden/web/controller/TestController.java
+++ src/main/java/com/garden/web/controller/TestController.java
@@ -3,6 +3,8 @@ package com.garden.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.garden.web.throwable.ResourceNotFoundException;
+
 @Controller
 @RequestMapping(value="/test")
 public class TestController {
@@ -10,6 +12,12 @@ public class TestController {
 	@RequestMapping(value="/home")
 	public String toHome(){
 		return "home";
+	}
+	
+	@RequestMapping(value="/getResource")
+	public void getResource(){
+		//...
+		throw new ResourceNotFoundException();
 	}
 	
 }
